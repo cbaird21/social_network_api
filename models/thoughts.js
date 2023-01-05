d07a5bc77772cdb5e2768c5d3f073a10087c0a2b
unknown <chandler.baird21@gmail.com>
2023-01-05 16:55:58 -0700

    finished thoughts schema

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,19 +1,42 @@
 const { Schema, Types } = require('mongoose');
+const moment = require('moment');
+
+// reactions(these are like replies)
+// array of nested documents created with the reactionSchema
+const reactionSchema = new schema (
+    {
+        // reactionId , mongoose ObjectId data type, default value is set to new ObjectId
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        //reactionBody, STRING REQUIRED MAX=280
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        // username STRING REQUIRED
+        username: {
+            type: String,
+            required: true,
+        },
+        // createdAt DATE, set default value to the current timestamp, 
+        // use a getter method to format the timestamp on query
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format("MMM Do, YYYY [at] hh:mm a")
+        }
+    }
+    )
 
 
 const thoughtSchema = new Schema({
-    // reactions(these are like replies)
-    // array of nested documents created with the reactionSchema
 
-    // i'm not sure that this was the right way... i'm trying!
-
-    reactionId: {
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId(),
-    },
     // thought Text must have
     // string, required, must be between 1-280 characters
-    responseBody: {
+    thoughtText: {
         type: String,
         required: true,
         minLength: 1,
@@ -23,17 +46,20 @@ const thoughtSchema = new Schema({
     // Date, set default value to the current timstamp,use a GETTER method to format the timestamp on a query
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format("MMM Do, YYYY [at] hh:mm a")
     },
     // username(the user that created this thought)
     // string and required
     username: {
         type: String,
         required: true,
-    }
+    },
+    reactions: [reactionSchema],
 },
     {
         toJSON: {
+            virtuals:true,
             getters: true,
         },
         id: false,
@@ -45,7 +71,7 @@ thoughtSchema
     .virtual('reactionCount')
 // getter function to get the length of the thoughts reactions array field on query.
     .get(function (){
-        return `${this.responseBody.length}`
+        return `${this.reactions.length}`
     });
 
 // initalize our Thoughts model
