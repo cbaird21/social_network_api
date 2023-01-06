2118c17528fd19a06ce6db778ae2eee624afa5c7
unknown <chandler.baird21@gmail.com>
2023-01-05 17:45:10 -0700

    updated user-routes

---
+++ Routes/api/user-routes.js
@@ -0,0 +1,29 @@
+const router = require('express').router();
+
+
+const {
+    getUser,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
+// api/users
+// Get all and Post
+router.route('/').get(getUser).post(createUser);
+
+// api/users/:userId
+// GET one user, PUT(UPDATE), AND DELETE
+router.route('/:userId')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser)
+
+// api/user/LuserId/friends:friendId 
+// POST and DELETE BY ID
+router.route('/:userId/friends/:friendsId')
+.post(addFriend)
+.delete(deleteFriend);
