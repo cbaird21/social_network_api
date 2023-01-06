63a3fa493fb01f1266f38f87afc4d4664f958d8f
unknown <chandler.baird21@gmail.com>
2023-01-05 18:24:44 -0700

    added index.js route

---
+++ Routes/index.js
@@ -0,0 +1,11 @@
+const router = require('express').router();
+
+// import all api routes from the api directory
+const apiRoutes = require('./api');
+
+// add prefix of '/api' to api routes
+router.use('/api', apiRoutes);
+
+router.use((req, res) => res.send('Wrong routes!'));
+
+module.exports = router;
