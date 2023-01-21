1b13ab9fddf7b0d49cb89268c2903cfd423a499f
unknown <chandler.baird21@gmail.com>
2023-01-21 08:51:52 -0700

    made some adjustments to userController to get routes working properly

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,16 @@ const { User, Thought } = require('../models')
 module.exports = {
     // GET all users
     async getUser(req, res) {
-        await User.find()
-            .then((user) => res.status(200).json(user))
-            .catch((err) => res.status(500).json(err));
+        try {
+            const users = await User.find({})
+                .populate('friends')
+                .populate('thoughts');
+
+            res.status(200).json(users)
+        }
+        catch (error) {
+            res.status(500).json(err)
+        }
     },
     // GET a user by id
     async getSingleUser(req, res) {
@@ -16,7 +23,7 @@ module.exports = {
             .then((user) =>
                 !user
                     ? res.status(404).json({ message: "No user found with that ID." })
-                    : res.json(user)
+                    : res.status(200).json(user)
             )
             .catch((err) => res.status(500).json(err));
     },
@@ -31,7 +38,7 @@ module.exports = {
     },
     // UPDATE a user
     async updateUser(req, res) {
-        await User.findOneAndUpdate(
+        await User.updateOne(
             { _id: req.params.userId },
             { $set: req.body },
             { runValidators: true, new: true }
@@ -39,13 +46,13 @@ module.exports = {
             .then((user) =>
                 !user
                     ? res.status(404).json({ message: "No user found with this ID" })
-                    : res.json(user)
+                    : res.status(200).json(user)
             )
             .catch((err) => res.status(500).json(err));
     },
     // DELETE a user
     async deleteUser(req, res) {
-        await User.findOneAndDelete({ _id: req.params.userId })
+        await User.deleteOne({ _id: req.params.userId })
             .then((user) =>
                 !user
                     ? res.status(404).json({ message: "No user found with this ID." })
@@ -57,9 +64,9 @@ module.exports = {
     },
     // add a freiend to a users friend list
     async addFriend(req, res) {
-        await User.findOneAndUpdate(
+        await User.updateOne(
             { id: req.params.userId },
-            { $addtoSet: { friends: req.params.friendId } },
+            { $push: { friends: req.params.friendId } },
             { runValidators: true, new: true }
         )
             .then((user) =>
@@ -71,7 +78,7 @@ module.exports = {
     },
     // delete friend
     async deleteFriend(req, res) {
-        await User.findOneAndUpdate(
+        await User.updateOne(
             { id: req.params.userId },
             { $pull: { friends: req.params.friendId } },
             { runValidators: true, new: true }
